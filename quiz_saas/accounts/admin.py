f0f32e78acad6f91587c69f484caf2366c289511
Godlovedev <kengnegodlove@gmail.com>
2024-12-21 16:16:28 +0100

    mise en placd du model user

--- quiz_saas/accounts/admin.py
+++ quiz_saas/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from accounts.models import User
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "is_premium", "total_quizzes_taken", "quiz_taken_today")
+
+admin.site.register(User, UserAdmin)
--- quiz_saas/accounts/models.py
+++ quiz_saas/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    is_premium = models.BooleanField(default=False)
+
+    quiz_taken_today = models.IntegerField(default=0)
+
+    total_quizzes_taken = models.IntegerField(default=0)
+
+    profile_picture = models.ImageField(upload_to="images", null=True, blank=True)
+
+    def __str__(self):
+        return self.username
