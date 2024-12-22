8a1732a14669c34e159fcca08fb5f2723fce97e5
Godlovedev <kengnegodlove@gmail.com>
2024-12-22 17:22:12 +0100

    definition des models pour l'application de quiz

--- quiz_saas/quiz/models.py
+++ quiz_saas/quiz/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Quiz(models.Model):
+    """ 
+    description: models for quiz
+    author: Godlove
+    """
+
+    title = models.CharField(max_length=30)
+
+    description = models.TextField(blank=True, null=True)
+    
+    creator = models.ForeignKey(User, related_name="quiz", on_delete=models.PROTECT)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    """
+    author: Godlove
+    description: this is the question model
+    """
+
+    quiz = models.ForeignKey(Quiz, related_name="question", on_delete=models.CASCADE)
+    
+    text = models.CharField(max_length=500)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
+
+
+class Choice(models.Model):
+
+    """
+    author: Godlove
+    description: Choice model definition
+    """
+
+    question = models.ForeignKey(Question, related_name="choice", on_delete=models.CASCADE)
+
+    text = models.CharField(max_length=200)
+
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class UserResult(models.Model):
+
+    """
+    author: Godlove
+    description: result model for user
+    """
+
+    user = models.ForeignKey(User, related_name="result", on_delete=models.CASCADE)
+
+    quiz = models.ForeignKey(Quiz, related_name= "result", on_delete=models.CASCADE)
+
+    score = models.FloatField(default=0)
+
+    completed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.quiz.id} - Score: {self.score}"
